47598a7d02d9031c338ad736159ef002e6000810
The-Graze <reece2004f@gmail.com>
2023-11-27 23:11:29 +0000

    Add project files.

---
+++ ThirdPersonHatFix/Plugin.cs
@@ -0,0 +1,22 @@
+﻿using BepInEx;
+using System;
+using UnityEngine;
+using Utilla;
+
+namespace ThirdPersonHatFix
+{
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        Plugin() => Events.GameInitialized += OnGameInitialized;
+        void OnGameInitialized(object sender, EventArgs e)
+        {
+            int mirrorOnlyMask = 1 << LayerMask.NameToLayer("MirrorOnly");
+            int noMirrorMask = 1 << LayerMask.NameToLayer("NoMirror");
+
+            Camera.allCameras[1].cullingMask |= mirrorOnlyMask;
+            Camera.allCameras[1].cullingMask &= ~noMirrorMask;
+        }
+    }
+}
---
+++ ThirdPersonHatFix/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace ThirdPersonHatFix
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.graze.gorillatag.thirdpersonhatfix";
+        public const string Name = "ThirdPersonHatFix";
+        public const string Version = "1.0.0";
+    }
+}
